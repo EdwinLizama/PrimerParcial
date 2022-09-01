28b65283616540696a2b708f6f9f51a4aa9af110
sandi <diego.morales20036@gmail.com>
2022-09-01 10:45:17 -0600

    mensaje

--- Inventario.js
+++ Inventario.js
@@ -1,35 +1,28 @@
-function indumentaria(name, id, precio, stock){
-    this.name = name
-    this.id = id
-    this.precio = precio
-    this.stock = stock
-    
+const Indumentaria = (name, id, precio, stock) =>{
+    this.name = name;
+    this.id = id;
+    this.precio = precio;
+    this.stock = stock;
+    return{ name, id, precio, stock };
+};
+const Camisa = (name, id, precio, stock) =>{
+    const prototype = Indumentaria(name, id, precio, stock);
+    return Object.assign({},prototype);
+}
+const Pantalon = (name, id, precio, stock) =>{
+    const prototype = Indumentaria(name, id, precio, stock);
+    return Object.assign({},prototype);
+}
+const Zapato = (name, id, precio, stock) =>{
+    const prototype = Indumentaria(name, id, precio, stock);
+    return Object.assign({},prototype);
 }
 
-function camisa(name, id, precio, stock){
-    indumentaria.call(this, name, id, precio, stock)
-}
+let camisa1 = Camisa("Camisa1", 1, 10, 2);
+let pantalon1 = Camisa("Camisa1", 1, 20, 2);
+let zapato1 = Camisa("Camisa1", 1, 15, 2);
 
-function pantalon(name, id, precio, stock){
-    indumentaria.call(this, name, id, precio, stock)
-}
 
-function zapato(name, id, precio, stock){
-    indumentaria.call(this, name, id, precio, stock)
-}
-
-camisa.prototype = Object.create(indumentaria.prototype)
-camisa.prototype.constructor = camisa
-
-pantalon.prototype = Object.create(indumentaria.prototype)
-pantalon.prototype.constructor = pantalon
-
-zapato.prototype = Object.create(indumentaria.prototype)
-zapato.prototype.constructor = zapato
-
-let camisa1 = new camisa("camisa", 1, 10, 2)
-let pantalon1 = new pantalon("pantalon", 2, 20, 2)
-let zapato1 = new zapato("zapato", 3, 30, 2)
 
 const tienda = (() =>{ 
     let inventario = [ 
@@ -41,6 +34,7 @@ const tienda = (() =>{
     const agregarProducto = (producto) =>{
         carritoCompras.push(producto)
         actualizarInventario(producto)
+        //mensaje
     }
     
     const realizarPago = (carrito) =>{
@@ -56,8 +50,80 @@ const tienda = (() =>{
         }
     }
 
-    return {agregarProducto, realizarPago}
+    return {inventario, carritoCompras, agregarProducto, realizarPago}
 })();
+
+let div1 = document.getElementById("inventario")
+let div2 = document.getElementById("carrito")
+//caja camisa
+let cajaCamisa = document.createElement("cajita")
+cajita.setAttribute("class", "camisa")
+//caja pantalon
+let cajaPantalon = document.createElement("cajita")
+cajita.setAttribute("class", "pantalon")
+//caja zapato
+let cajaZapato = document.createElement("cajita")
+cajita.setAttribute("class", "zapato")
+
+//botones
+let botonAgregarCamisa = document.createElement("btn1")
+let botonAgregarPantalon = document.createElement("btn2")
+let botonAgregarZapato = document.createElement("btn3")
+
+function mostrar(){
+    tienda.inventario.foreach(producto =>{
+        for (let index = 0; index < producto.stock; index++) {
+            if(producto.name == "camisa"){
+            div1.appendChild(cajaCamisa)
+            }
+            if(producto.name == "pantalon"){
+            div1.appendChild(cajaPantalon)
+            }
+            if(producto.name == "zapato"){
+            div1.appendChild(cajaZapato)
+            }
+        }
+    })
+    
+    tienda.carrito.foreach(producto =>{
+        for (let index = 0; index < producto.stock; index++) {
+            if(producto.name == "camisa"){
+            div2.appendChild(cajaCamisa)
+            }
+            if(producto.name == "pantalon"){
+            div2.appendChild(cajaPantalon)
+            }
+            if(producto.name == "zapato"){
+            div2.appendChild(cajaZapato)
+            }
+        }
+    })
+}
+mostrar()
+
+
+
+botonAgregarCamisa.addEventListener("click", () =>{
+    agregarProducto(inventario[0])
+    div1.innerHTML=''
+    div2.innerHTML=''
+    mostrar()
+})
+botonAgregarPantalon.addEventListener("click", () =>{
+    agregarProducto(inventario[1])
+    div1.innerHTML=''
+    div2.innerHTML=''
+    mostrar()
+})
+botonAgregarZapato.addEventListener("click", () =>{ 
+    agregarProducto(inventario[2])
+    div1.innerHTML=''
+    div2.innerHTML=''
+    mostrar()
+})
+
+
+ 
 
 
 
